c9323bb5de765ad3ff3b55e1849975abecc20b1f
Andrew Arena <aja298@cornell.edu>
2025-03-28 17:52:44 -0400

    Add CLI flags for specifying flow in and initial traffic

--- traffic_sim/bin/main.ml
+++ traffic_sim/bin/main.ml
@@ -1,5 +1,4 @@
 open Traffic_sim
-open Core.Command
 open Command_unix
 
 let () = Random.self_init ()
@@ -41,12 +40,31 @@ let get_rate = function
 let parse_lane_rates lr =
   List.to_array (List.map (String.split lr ~on:' ') ~f:float_of_string)
 
-let get_ask_for_rates = function
+let get_ask_for = function
   | None -> false
   | Some s -> s
 
+let parse_traffic_string str =
+  let strlst =
+    match str with
+    | None -> []
+    | Some k -> String.to_list k
+  in
+  let f elem =
+    match elem with
+    | 'R' -> Car.Car.right_car
+    | 'L' -> Car.Car.left_car
+    | 'S' -> Car.Car.straight_car
+    | '*' -> Car.Car.random_car ()
+    | k ->
+        raise
+          (Invalid_argument
+             ("Unexpected character " ^ String.of_char k ^ " when entering cars"))
+  in
+  List.map strlst ~f
+
 let get_rates_from_cl ask_for_rates =
-  if get_ask_for_rates ask_for_rates then (
+  if ask_for_rates then (
     let arr = [| 0.0; 0.0; 0.0; 0.0 |] in
     print_endline "Enter North Rate:";
     arr.(0) <- get_rate (read_float_opt ());
@@ -59,13 +77,35 @@ let get_rates_from_cl ask_for_rates =
     arr)
   else [| 0.2; 0.2; 0.2; 0.2 |]
 
+let get_traffic_from_cl ask_for_traffic =
+  if ask_for_traffic then (
+    let arr = [| []; []; []; [] |] in
+    print_endline
+      "Enter initial traffic in each lane. \n\
+       (R) is a right-turning car, \n\
+       (L) is left-turning, \n\
+       (S) is going straight, \n\
+       (*) is a random turn direction.\n\
+       Enter as one string without any other characters (e.g. SSLRR*) ";
+    print_endline "Enter North Traffic:";
+    arr.(0) <- parse_traffic_string In_channel.(input_line stdin);
+    print_endline "Enter East Traffic:";
+    arr.(1) <- parse_traffic_string In_channel.(input_line stdin);
+    print_endline "Enter South Traffic:";
+    arr.(2) <- parse_traffic_string In_channel.(input_line stdin);
+    print_endline "Enter West Traffic:";
+    arr.(3) <- parse_traffic_string In_channel.(input_line stdin);
+    arr)
+  else [| []; []; []; [] |]
+
 (** Make sure the sps arg is > 0 !!! *)
-let run (sps, ask_for_rates) =
+let run sps ask_for_rates ask_for_traffic =
   try
     if sps < 0 then raise (Invalid_argument "Sps must lowkey be positive.");
     let rates = get_rates_from_cl ask_for_rates in
+    let cars = get_traffic_from_cl ask_for_traffic in
     TheView.TheView.render
-      (Intersection.create [| []; []; []; [] |] rates)
+      (Intersection.create cars rates)
       (* (Intersection.create [| []; []; gencars; gencars |] [| 0.0; 0.0; 0.0;
          0.0 |]) *)
       sps
@@ -74,11 +114,10 @@ let run (sps, ask_for_rates) =
 let command =
   Command.basic ~summary:"Traffic Simulation - 4-Way Intersection"
     ~readme:(fun () -> help)
-    Command.Param.(
-      map
-        (both
-           (anon ("Steps_per_second" %: int))
-           (anon (maybe ("Set_flow_rate_into_lanes" %: bool))))
-        ~f:(fun sps () -> run sps))
+    (let%map_open.Command useflow =
+       flag "-f" no_arg ~doc:"specify traffic flow in"
+     and usetraffic = flag "-t" no_arg ~doc:"specify initial traffic"
+     and sps = anon (maybe_with_default 5 ("Steps per second" %: int)) in
+     fun () -> run sps useflow usetraffic)
 
 let () = Command_unix.run ~version:"1.0" command
